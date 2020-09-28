fc878093f7b8a48868485cc216f252218d2883e7
Marc <phyrenight@gmail.com>
2020-09-28 16:30:54 -0400

    feature: right button does not go to next slide

---
+++ js/app.js
@@ -0,0 +1,29 @@
+!(function(d) {
+    let slides = d.getElementsByClassName('carousel__container');
+    let slides_length = slides.length;
+    let right_button = d.getElementsByClassName('carousel__button--right')[0];
+    let left_button = d.getElementsByClassName('carousel__button--left')[0];
+    let current_slide = 0;
+    let prev_slide = slides_length;
+
+    right_button.onclick  = function() {
+        prev_slide = current_slide;
+        if(current_slide >=  (slides_length - 1)){
+            current_slide = 0;
+        }else {
+            current_slide += 1; 
+        }
+        update_page()
+    }
+
+
+    function update_page() {
+     console.log("current " +current_slide)
+     console.log("prev " +prev_slide)
+        slides[prev_slide].classList.remove("active")
+        slides[current_slide].classList.add("active");
+       // slides[prev_slide].classList.add("prev");
+
+    }
+
+}(document))
